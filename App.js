bfec94637bbc63085afb80b72843590684259acb
Matheus da Silva Corsine <matheuscorsine4129@gmail.com>
2025-05-27 21:23:48 -0400

    criando repositório novo

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
+  const [heartColor, setHeathColor] = useState('gray');
+
+  const toggleHeartColor = () => {
+    setHeathColor((prevColor) => (prevColor === 'red' ? 'gray' : 'red'));
+  };
+
+  const showAlert = () => {
+    try {
+      alert('Clicou mesmo!');
+    } catch (error) {
+      console.error('Error no showing alert' + error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Image
+        source={require('./assets/minion.png')} //caminho da imagem
+        style={{ width: 200, height: 200 }} //tamanho da imagem 
+      />
+
+      <Text>matheusin1234567</Text>
+      
+      <TextInput 
+        style= {styles.input}
+        placeholder='Escreva algo...'
+        placeholderTextColor='#544b4b'
+      />
+
+      <TouchableOpacity onPress={toggleHeartColor}>
+        <AntDesign name="heart" size={24} color={heartColor} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={showAlert} style={styles.customButton}>
+        <Text style={styles.buttonText}>Clica ai</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -17,4 +52,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 10
+  },
+
+  customButton: {
+    backgroundColor: "green",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderRadius: 8
+  },
+
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: '600'
+  },
+
+  input: {
+    height: 40,
+    width: '50%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 7,
+    marginVertical: 10,
+    borderRadius: 5
+  }
 });
